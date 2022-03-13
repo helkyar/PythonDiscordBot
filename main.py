283e88d0baa3d9aeb5f4449ec6ed00b1ee953480
Javier Palacios <89964202+helkyar@users.noreply.github.com>
2022-03-13 22:07:15 +0000

    Add basic functionality

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import discord
+
+client = discord.Client()
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(msg):
+  if msg.author == client.user:
+    return
+
+  if msg.content.startswith('$hello'):
+    await msg.channel.send('Hello!')
+
+client.run(os.environ['TOKEN'])
