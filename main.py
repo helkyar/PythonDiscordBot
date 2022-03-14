05deb0044263622d5e466d5f435e48de3d649ee1
Javier Palacios <89964202+helkyar@users.noreply.github.com>
2022-03-14 03:19:57 +0000

    Add api connection

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import os
 import discord
+import requests
+import json
+
+api_url  = "https://zenquotes.io/api/random"
+sad_words = ["sad", "depressed", "unhappy", "angry", "misserable"]
+cheers = ["Cheer up!","You are a great person / bot"]
 
 client = discord.Client()
+
+def get_quote():
+  response = requests.get(api_url)
+  json_data = json.loads(response.text)
+  quote = f'{json_data[0]["q"]} - {json_data[0]["a"]}'
+  return quote
+
 @client.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+  print(f'We have logged in as {client.user}')
 
 @client.event
 async def on_message(msg):
   if msg.author == client.user:
     return
 
-  if msg.content.startswith('$hello'):
-    await msg.channel.send('Hello!')
+  if msg.content.startswith('$inspire'):
+    quote = get_quote()
+    await msg.channel.send(quote)
 
 client.run(os.environ['TOKEN'])
