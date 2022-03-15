2dd462b50fa6c8401bfc3911c96b3bf542b3f749
Javier Palacios <89964202+helkyar@users.noreply.github.com>
2022-03-15 22:35:14 +0000

    Add database connection

--- main.py
+++ main.py
@@ -3,18 +3,37 @@ import discord
 import requests
 import json
 import random
+from replit import db
 
 api_url  = "https://zenquotes.io/api/random"
 sad_words = ["sad", "depressed", "unhappy", "angry", "misserable"]
 cheers = ["Cheer up!","You are a great person / bot"]
 
-client = discord.Client()
-
+#================= API CONNECTION =====================
 def get_quote():
   response = requests.get(api_url)
   json_data = json.loads(response.text)
   quote = f'{json_data[0]["q"]} - {json_data[0]["a"]}'
   return quote
+  
+#=============== DATABASE CONNECTION ==================
+def update_cheers(cheer_msg):
+  if "cheers" in db.keys():
+    cheers = db["cheers"]
+    cheers.append(cheer_msg)
+    db["cheers"] = cheers
+  else:
+     db["cheers"] = cheer_msg
+
+def delete_cheer(index):
+  cheers = db["cheers"]
+  
+  if len(cheers) > index:
+    del cheers[index]
+  db["cheers"] = cheers
+
+#=============== DISCORD CONNECTION ===================
+client = discord.Client()
 
 @client.event
 async def on_ready():
